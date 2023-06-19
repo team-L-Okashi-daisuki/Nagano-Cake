a82620ee2ae9ea6a901951bd0005837302616e87
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-06-19 04:55:18 +0000

    public-orders

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,17 @@ Rails.application.routes.draw do
   
   
   namespace :public do
-    get 'orders/index'
-    get 'orders/show'
-    get 'orders/new'
+   
     get 'orders/log'
     get 'orders/thanks'
   end
   devise_for :users
   
-  resources :orders, only: []
+  resources :orders, only: [:new, :create, :index, :show] do
+    post "log" => "orders#log"
+    get "thanks" => "orders#thanks"
+  end
+  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
