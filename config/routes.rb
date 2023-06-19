22ee3cc8539e00b207b644d3b38b70eaca123961
EC2 Default User <ec2-user@ip-172-31-6-61.ap-northeast-1.compute.internal>
2023-06-19 12:56:58 +0000

    routeerroer

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
+  
   # homes
   root to: 'homes#top'
   get 'homes/about' => "homes#about"
-  
-  # items
-  get 'items' => "items#index"
-  get 'items/show'
-  
-  
+
+  scope module: 'public' do
+    # items
+    get 'items' => "items#index"
+    get 'items/show'
+  end
+
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
