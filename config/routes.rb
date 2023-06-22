d3b7d485412fca1e283ef1a67644055884732cb4
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-06-22 10:24:55 +0000

    develop

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,28 @@
 Rails.application.routes.draw do
 
+namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'orders/show'
+  end
+
+  root to: 'homes#top'
+  get 'homes/about' => "homes#about"
+
+  devise_for :customers, skip: [:passwords],  controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  scope module: 'public' do
+    get 'customers/show'
+    get 'customers/information/edit' => "customers#edit"
+    get 'customers/unsubscribe'
+    patch 'customers/withdraw' => "customers#withdraw"
+    resources :customers, only: [:update]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
