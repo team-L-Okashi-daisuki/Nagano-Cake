4bfa20d5af0df26000370be6ce94685eb8c41bb1
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-06-25 07:46:57 +0000

    up

--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ Rails.application.routes.draw do
   end
   
   scope module: 'public' do
-    
+     resources :shippings, only: [:index, :create, :destroy, :edit, :update]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
