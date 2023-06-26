b96e461d15cda572c7bf0dbbcde719faec5c5aa4
EC2 Default User <ec2-user@ip-172-31-37-157.ap-northeast-1.compute.internal>
2023-06-26 05:53:48 +0000

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,39 @@
 Rails.application.routes.draw do
 
   #devise_for :admins
-  devise_for :admin, controllers: {
-  sessions:      'admin/sessions',
-  passwords:     'admin/passwords',
-  registrations: 'admin/registrations'
-}
-namespace :admin do
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+  }
+  devise_for :customers, skip: [:passwords],  controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  namespace :admin do
     resources :orders, only: [:show, :update]
     get "/" => "homes#top"
     resources :customers, only: [:index, :show, :edit, :update]
     resources :genres, only: [:index, :edit, :create, :update]
     resources :products, only: [:index, :create, :new, :show, :edit, :update]
     resources :order_details, only: [:update]
+
     get "/search" => "products#search"
+
 
 
   end
 
-  root to: 'homes#top'
-  get 'homes/about' => "homes#about"
-
-  devise_for :customers, skip: [:passwords],  controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
-
   scope module: 'public' do
+    root "homes#top"
+    get 'homes/about' => "homes#about"
     get 'customers/show'
     get 'customers/information/edit' => "customers#edit"
     get 'customers/unsubscribe'
     patch 'customers/withdraw' => "customers#withdraw"
     resources :customers, only: [:update]
     resources :items, only: [:index, :show]
-    resources :cart_items, only: [:idex, :update, :destroy, :create]
+    delete "/cart_items/destroy_all" => "cart_items#all_destroy"
+    resources :cart_items, only: [:index, :update, :destroy, :create]
 
   end
 
@@ -42,6 +42,10 @@ namespace :admin do
       post "confirm" => "orders#confirm"
       get "thanks" => "orders#thanks"
     end
+  end
+
+  scope module: 'public' do
+     resources :shippings, only: [:index, :create, :destroy, :edit, :update]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
